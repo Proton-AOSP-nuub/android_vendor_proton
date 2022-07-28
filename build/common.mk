ed6e14af5d727d87a4286ef5aef0b11170e09d8d
SagarMakhar <sagarmakhar@gmail.com>
2022-07-28 07:24:06 +0530

    Build Black theme [4/4]

--- build/common.mk
+++ build/common.mk
@@ -20,6 +20,10 @@ PRODUCT_HOST_PACKAGES += \
     signapk \
     zipalign
 
+# Overlays
+PRODUCT_PACKAGES += \
+    AndroidBlackThemeOverlay
+
 # Disable remote keyguard animation
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.wm.enable_remote_keyguard_animation=0
